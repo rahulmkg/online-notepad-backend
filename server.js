23674eed74a31fa1d7e1889dc1075c7a0638d1da
rahulmkg <engg.himanshu007@gmail.com>
2026-01-18 23:19:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TEMP in‑memory notes (abhi DB nahi)
+let notes = [];
+
+// test route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
+});
+
+// get all notes
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+// save note
+app.post("/notes", (req, res) => {
+  const note = {
+    id: Date.now(),
+    title: req.body.title,
+    content: req.body.content
+  };
+  notes.push(note);
+  res.json(note);
 });
 
 const PORT = process.env.PORT || 10000;
